9cc5cf7e04123187e99046fa22b2f928192dd46e
Krzysztof Mędrzycki <aiwenari@gmail.com>
2013-03-11 16:34:59 +0100

    Added install script.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##
+# aiwenár - dotfiles install script
+#
+
+cd $(dirname $0)
+
+pwd=`pwd`
+ln -s $pwd/profile $HOME/.profile
+ln -s $pwd/scripts $HOME/.scripts
+ln -s $pwd/startup.py $HOME/.startup.py
+ln -s $pwd/zshrc $HOME/.zshrc
